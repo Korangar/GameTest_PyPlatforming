7a423b2f3a586f8aa0e09f30dd65a12fd4fa4f6f
LEM-II\Ilja <>
2017-02-27 22:28:24 +0100

    changes to xinput module: state is fully encapsulated in  a dictionary statechanges result in events that are saved as part of an input state events are collected in a dictionary
    resulting changes to input module:
    refactoring to suit to the new xinput module
    player commands now are triggered by button statechange events instead of simply their current state

--- input.py
+++ input.py
@@ -1,13 +1,12 @@
-from xinput import XGamepad
+from xinput import XGamepad, AnalogStick
 from pygame.math import Vector2
 from player import Player
-
-gamepads = list(XGamepad)
 
 
 # update function for input
 def poll(player: list):
     XGamepad.update()
+    gamepads = list(XGamepad)
     for i in range(4):
         pad = gamepads[i]  # type: XGamepad
         if not pad.connected:
@@ -17,8 +16,14 @@ def poll(player: list):
         else:
             if not player[i]:
                 player[i] = Player()
-            player[i].player_directional(Vector2() + (pad.analog_left_x, pad.analog_left_y))
-            if pad.button_a:
+
+            stick_left = pad.input_state["analog_left"]  # type: AnalogStick
+            player[i].player_directional(Vector2() + (stick_left.x, stick_left.y))
+
+            events = pad.input_state["event"]
+            print(events)
+            if "button_a" in events and events["button_a"]:
                 player[i].jump()
-            if pad.button_x:
+
+            if "button_x" in events and events["button_x"]:
                 player[i].shoot()
